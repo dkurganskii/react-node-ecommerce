9a798a53446b627551435e8092493a7f38916095
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-11 15:24:48 -0500

    implement error handling

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 // app
 const app = express();
@@ -13,6 +16,11 @@ mongoose
 mongoose.connection.on('error', (err) => {
 	console.log(`DB connection error: ${err.message}`);
 });
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // routes middleware
 app.use('/api', userRoutes);
--- models/user.js
+++ models/user.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
-const uuidv1 = require('./uuid/v1');
+const uuidv1 = require('uuidv1');
 
 const userSchema = new mongoose.Schema(
 	{
 		name: {
 			type: String,
 			trim: true,
-			requied: true,
+			required: true,
 			maxlength: 32
 		},
 		email: {
 			type: String,
 			trim: true,
-			requied: true,
+			required: true,
 			unique: true
 		},
 		hashed_password: {
 			type: String,
-			requied: true
+			required: true
 		},
 		about: {
 			type: String,
@@ -36,3 +36,28 @@ const userSchema = new mongoose.Schema(
 	},
 	{ timestamps: true }
 );
+
+// virtual field
+userSchema
+	.virtual('password')
+	.set(function(password) {
+		this._password = password;
+		this.salt = uuidv1();
+		this.hashed_password = this.encryptPassword(password);
+	})
+	.get(function() {
+		return this._password;
+	});
+
+userSchema.methods = {
+	encryptPassword: function(password) {
+		if (!password) return '';
+		try {
+			return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+		} catch (err) {
+			return '';
+		}
+	}
+};
+
+module.exports = mongoose.model('User', userSchema);
