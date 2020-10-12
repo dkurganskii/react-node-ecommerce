79083f62100e8d85587eb4693a1a39c9cda77610
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-12 13:55:17 -0500

    implement category and product functionulity

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const cookieParser = require('cookie-parser');
 // import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
 
 // app
 const app = express();
@@ -28,6 +30,8 @@ app.use(cookieParser());
 // routes middleware
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
 
 const port = process.env.PORT || 8000;
 
