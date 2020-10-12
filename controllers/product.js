b5f86e1933ee7fee6ccd73879f9db858ff51acec
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-12 14:21:31 -0500

    add product validation

--- controllers/product.js
+++ controllers/product.js
@@ -11,9 +11,20 @@ exports.create = (req, res) => {
 		if (err) {
 			return res.status(400).json({ error: 'Image could not be uploaded' });
 		}
+
+		// check for all fields
+		const { name, description, price, quantity, shipping, category } = fields;
+		if (!name || !description || !price || !quantity || !category || !shipping) {
+			return res.status(400).json({ error: 'All fields are required' });
+		}
+
 		let product = new Product(fields);
 
 		if (files.photo) {
+			// console.log('FILES PHOTO: ' + files.photo);
+			if (files.photo.size > 1000000) {
+				return res.status(400).json({ error: 'Image should be less than 1mb in size' });
+			}
 			product.photo.data = fs.readFileSync(files.photo.path);
 			product.photo.contentType = files.photo.type;
 		}
