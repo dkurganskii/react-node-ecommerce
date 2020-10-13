cf05eb1532397c4b2e79d6eb2f9f49506d2c5a55
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-13 06:50:03 -0500

    add functionulity to update product and get category

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove } = require('../controllers/product');
+const { create, productById, read, remove, update } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const { route } = require('./auth');
@@ -9,6 +9,7 @@ const { route } = require('./auth');
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
 
 router.param('userId', userById);
 router.param('productId', productById);
