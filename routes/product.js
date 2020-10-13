a3eae597ad10ed0ac716917f7b60d0d8a9493baa
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-13 11:30:00 -0500

    add functionality to get list of products by search

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated } = require('../controllers/product');
+const {
+	create,
+	productById,
+	read,
+	remove,
+	update,
+	list,
+	listRelated,
+	listCategories,
+	listBySearch
+} = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const { route } = require('./auth');
@@ -13,6 +23,8 @@ router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update
 
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+router.post('/products/by/search', listBySearch);
 
 router.param('userId', userById);
 router.param('productId', productById);
