82033a2e2808c0a0fa35589fabae4d019a9eb171
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-14 06:54:36 -0500

    add bootstrap and create menu component

--- ecommerce-front/src/Routes.js
+++ ecommerce-front/src/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './core/Home';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
+import Menu from './core/Menu';
 
 const Routes = () => {
 	return (
 		<BrowserRouter>
+			<Menu />
 			<Switch>
-				<Route path="/home" exact component={Home} />
+				<Route path="/" exact component={Home} />
 				<Route path="/signin" exact component={Signin} />
 				<Route path="/signup" exact component={Signup} />
 			</Switch>
