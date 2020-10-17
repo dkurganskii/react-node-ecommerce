86ea3a5ff0f1edf4d06b6759f71d6db6b5f73405
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-17 06:50:50 -0500

    show categories in shop sidebar

--- ecommerce-front/src/core/Shop.js
+++ ecommerce-front/src/core/Shop.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import Card from './Card';
 import { getCategories } from './ApiCore';
+import Checkbox from './Checkbox';
 
 const Shop = () => {
 	const [ categories, setCategories ] = useState([]);
@@ -24,7 +25,12 @@ const Shop = () => {
 	return (
 		<Layout title="Shop Page" description="Search and find books of your choice" className="container-fluid">
 			<div className="row">
-				<div className="col-4">{JSON.stringify(categories)}</div>
+				<div className="col-4">
+					<h4>Filter by categories</h4>
+					<ul>
+						<Checkbox categories={categories} />
+					</ul>
+				</div>
 				<div className="col-8">right</div>
 			</div>
 		</Layout>
