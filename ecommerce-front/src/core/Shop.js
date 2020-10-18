c3f05f1123597faa9a4ce108977fbed44f7f622d
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-18 06:18:29 -0500

    show products by filter on shop page

--- ecommerce-front/src/core/Shop.js
+++ ecommerce-front/src/core/Shop.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import Card from './Card';
-import { getCategories } from './ApiCore';
+import { getCategories, getFilteredProducts } from './ApiCore';
 import Checkbox from './Checkbox';
 import RadioBox from './RadioBox';
 import { prices } from './FixedPrices';
@@ -12,6 +12,9 @@ const Shop = () => {
 	});
 	const [ categories, setCategories ] = useState([]);
 	const [ error, setError ] = useState(false);
+	const [ limit, setLimit ] = useState(6);
+	const [ skip, setSkip ] = useState(0);
+	const [ filteredResults, setFilteredResults ] = useState(0);
 
 	const init = () => {
 		getCategories().then((data) => {
@@ -23,8 +26,20 @@ const Shop = () => {
 		});
 	};
 
+	const loadFilteredResults = (newFilters) => {
+		// console.log(newFilters);
+		getFilteredProducts(skip, limit, newFilters).then((data) => {
+			if (data.error) {
+				setError(data.error);
+			} else {
+				setFilteredResults(data);
+			}
+		});
+	};
+
 	useEffect(() => {
 		init();
+		loadFilteredResults(skip, limit, myFilters.filters);
 	}, []);
 
 	const handleFilters = (filters, filterBy) => {
@@ -36,6 +51,9 @@ const Shop = () => {
 			let priceValues = handlePrice(filters);
 			newFilters.filters[filterBy] = priceValues;
 		}
+
+		loadFilteredResults(myFilters.filters);
+
 		setMyfilters(newFilters);
 	};
 
@@ -67,7 +85,7 @@ const Shop = () => {
 						<RadioBox prices={prices} handleFilters={(filters) => handleFilters(filters, 'price')} />
 					</div>
 				</div>
-				<div className="col-8">{JSON.stringify(myFilters)}</div>
+				<div className="col-8">{JSON.stringify(filteredResults)}</div>
 			</div>
 		</Layout>
 	);
