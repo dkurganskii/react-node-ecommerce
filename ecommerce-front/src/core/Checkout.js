8fa0085fd1b440ba2cbeb97f414446269c723e3e
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-21 11:55:34 -0500

    process payment backend

--- ecommerce-front/src/core/Checkout.js
+++ ecommerce-front/src/core/Checkout.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Layout from './Layout';
+
 import { getBrainTreeClientToken } from './ApiCore';
-import Card from './Card';
+
 import {isAuthenticated} from '../auth'
 import DropIn from 'braintree-web-drop-in-react'
 
@@ -53,14 +53,41 @@ const showCheckout =()=>{
     )
 }
 
+const buy = ()=>{
+    // send the nonce to your server
+    // nonce = data.instance.requestPaymentMethod()
+    let nonce;
+    let getNonce = data.instance.requestPaymentMethod()
+    .then(data =>{
+        console.log(data)
+        nonce = data.nonce
+        // once you have nonce (cart type, cart number) send nonce as paymentMethodNonce
+        // and also total to be charged
+        console.log('send nonce and total to process: ', nonce, getTotal(products))
+    })
+    .catch(error =>{
+        console.log('dropin error: ', error);
+        setData({...data, error: error.message})
+    })
+}
+
+const showError = error => (
+    <div
+        className="alert alert-danger"
+        style={{ display: error ? "" : "none" }}
+    >
+        {error}
+    </div>
+);
+
 const showDropIn = ()=>(
-    <div>
+    <div onBlur={() => setData({ ...data, error: "" })}>
         {data.clientToken !== null && products.length > 0 ? (
             <div>
                 <DropIn options={{
                     authorization: data.clientToken
                 }} onInstance={instance =>(data.instance = instance)}/>
-                <button className="btn btn-success">Checkout</button>
+                <button onClick={buy} className="btn btn-success">Pay</button>
                 </div>
         ) : null}
     </div>
@@ -68,6 +95,7 @@ const showDropIn = ()=>(
 
 return <div>
     <h2>Total: ${getTotal()}</h2>
+    {showError(data.error)}
   {showCheckout()}
 </div>
 }
