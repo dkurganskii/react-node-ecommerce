26233a6391e94e2a3a0fdc8cde5db7c99c2732b3
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-23 16:06:48 -0500

    update order status by admin

--- ecommerce-front/src/admin/Orders.js
+++ ecommerce-front/src/admin/Orders.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
-import { listOrders, getStatusValues } from './ApiAdmin';
+import { listOrders, getStatusValues, updateOrderStatus } from './ApiAdmin';
 import moment from 'moment'
 
  const Orders =()=>{
@@ -55,7 +55,15 @@ const showOrdersLength = ()=>{
 }
 
 const handleStatusChange = (e, orderId) => {
-    console.log("update order status");
+    updateOrderStatus(user._id, token, orderId, e.target.value).then(
+        data => {
+            if (data.error) {
+                console.log("Status update failed");
+            } else {
+                loadOrders();
+            }
+        }
+    );
 };
 
 const showStatus = o => (
