fc2f2d787d81d91147fdaede5c5d1de9739d4d70
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-24 08:29:10 -0500

    get user info for profile update

--- ecommerce-front/src/user/Profile.js
+++ ecommerce-front/src/user/Profile.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import {read, update, updateUser} from './ApiUser'
 
-const Profile = ()=>{
-    return(
-        <h2>profile</h2>
-    )
+const Profile = (props)=>{
+  
+       const [values, setValues] = useState({
+           name: '',
+           email: '',
+           password: '',
+           error: false,
+           success: false
+       })
+       const {token} = isAuthenticated
+       const {name, email, password, error, success} = values
+
+    const init = (userId)=>{
+    read(userId, token).then(data=>{
+        if(data.error){
+      setValues({...values, error: true})
+        }else{
+            setValues({...values, name: data.name, email: data.email})
+        }
+    })
+    }
+
+    useEffect(()=>{
+        init(props.match.params.userId)
+    }, [])
+    return (
+		<Layout title="Profile" description="Update your profile" className="container-fluid">
+		<h2 className='mb-4'>Profile update</h2>
+        {JSON.stringify(values)}
+		</Layout>
+	);
 }
 
 export default Profile;
