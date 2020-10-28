b6b6d2900c4e868b1eef153a0af0603b995c58d2
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-10-28 13:51:26 -0500

    refactor server

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree')
+const orderRoutes = require('./routes/order')
+
+// app
+ const app = express();
+
+// db
+mongoose
+	.connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true })
+	.then(() => console.log('DB Connected'));
+mongoose.connection.on('error', (err) => {
+	console.log(`DB connection error: ${err.message}`);
+});
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// routes middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+app.use('/api', braintreeRoutes);
+app.use('/api', orderRoutes);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+// Set static folder
+app.use(express.static('client/build'))
+
+app.get('*', (req, res)=> {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+}
+
+
+export default app;
--- server.js
+++ server.js
@@ -1,57 +1,6 @@
-const express = require('express');
-require('dotenv').config();
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+ import app from './app';
 
-// import routes
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const braintreeRoutes = require('./routes/braintree')
-const orderRoutes = require('./routes/order')
-
-// app
-const app = express();
-
-// db
-mongoose
-	.connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true })
-	.then(() => console.log('DB Connected'));
-mongoose.connection.on('error', (err) => {
-	console.log(`DB connection error: ${err.message}`);
-});
-
-// middlewares
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors());
-
-// routes middleware
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', productRoutes);
-app.use('/api', braintreeRoutes);
-app.use('/api', orderRoutes);
-
-// Serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
-// Set static folder
-app.use(express.static('client/build'))
-
-app.get('*', (req, res)=> {
-res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
-}
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server started on PORT ${port}`));
